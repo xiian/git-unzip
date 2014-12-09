3507a4c4efc21e701970a5bc33ddeae0b0c20943
Tom Sartain <tomsartain@gmail.com>
2014-12-08 20:05:40 -0500

    Make sure we always have a string to work with

--- unzip/utils.py
+++ unzip/utils.py
@@ -31,6 +31,8 @@ def run_cmd(cmd, debug=True, multi=False):
     else:
         process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout, stderr = process.communicate()
+    stdout = stdout or ''
+    stderr = stderr or ''
     if process.returncode:
         raise GitCmdException(stdout=stdout, stderr=stderr, message='Problem running %s: %s' % (cmd, stderr.strip()))
     return stdout.strip()
